8b26c7eff2de2c0a97989ef14a9c1407dd774289
Hugo Romero <hugomrj@gmail.com>
2026-06-24 21:56:29 -0300

    inicial

---
+++ src/main/java/mimbre/HomeController.java
@@ -0,0 +1,19 @@
+package mimbre;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.View;
+import java.util.Map;
+
+@Controller("/")
+public class HomeController {
+
+    @View("prueba") // Buscará src/main/resources/views/prueba.html
+    @Get
+    public Map<String, Object> index() {
+        return Map.of(
+                "titulo", "Prueba de Mimbre",
+                "mensaje", "Hola! El template Pebble está funcionando correctamente."
+        );
+    }
+}
